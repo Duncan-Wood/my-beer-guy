844bba349e376803b61b512ad81a62da433fbea3
Duncan-Wood <duncanwoodpro@gmail.com>
2024-05-13 10:54:07 -0400

    fixed logo thing. They didnt load before because we were only permitting images from the google cloud bucket. I changed it so the images can be used from any url.

--- next.config.js
+++ next.config.js
@@ -8,9 +8,6 @@ module.exports = {
         contentDispositionType: 'attachment',
         contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
         remotePatterns: [
-          // need to add this to config to use optimized Next images
-          // should be worth it considering all logos come from Google Cloud Storage
-          // this is for our GCP bucket
           {
             protocol: 'https',
             hostname: '**',
